a2bde9bc1ff654b28aeb5711d9796a8b5f32f30f
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-28 10:00:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ if (!$result){
                 <div class="seperator">
 
                 </div>
-                <div class="list w3-row">
+                <div class="list">
                     <div class="" id="Lego"><h2>Lego</h2>
                     <?php
      require_once './functions.php';
@@ -166,32 +166,15 @@ if (!$result){
         $iStatus = $row['iStatus'];
         $iSize = $row['iSize'];
         $iImage = $row['iImage'];
-      }
+      
         
-        echo "<div class='sp w3-quarter w3-card w3-center ' ><div class='w3-orange w3-padding-large'>$iStatus</div><div ><img onclick=\"document.getElementById('$iName').style.display='block'\" id='testimg' src='". $iImage . "' width='100%'></div><div class='name'><h3>$iName</h3></div><h3>$iPrice$</h3></div>"
-                . "<!--SHOW MORE INFORMATION-->
-  <div id='$iName' class='w3-modal'>
-      <div class='w3-modal-content w3-animate-top w3-card-4'>
-        <div class='w3-container w3-orange w3-center w3-padding-20'> 
-          <span onclick=\"document.getElementById('$iName').style.display='none';\"
-         class='w3-button w3-orange w3-xlarge w3-display-topright'>×</span>
-          <h2>$iName</h2>
-        </div>
-        <div class='w3-container w3-row'>
-          <div class='w3-half'>
-              <img src='". $iImage . "' width='100%'>
-          </div>
-          <div class='w3-half w3-left'>
-              <h3> Price: $iPrice$</h3>
-              <p> Description: $iDescription.</p>
-              <h4>$iSize</h4>                           
-          </div>                                                    
-        </div>
-        <button class='w3-button w3-orange w3-section' onclick=\"document.getElementById('$iName').style.display='none';\">Close <i class='fa fa-remove'></i></button>
-      </div>
-    </div>";                                                                                       
-    }
-?>
+        echo "<div>$iStatus</div>
+        <div ><img src='". $iImage . "' width='100%'></div>
+        <div class='name'><h3>$iName</h3></div>
+        <div><h3>$iPrice$</h3></div>";  
+        } 
+        ?>                                                                                    
+
      
                 </div>        
                    
